39d67edc590e473cf531ba4f7836d580de056123
kurtreyn <kurtreyn@yahoo.com>
2023-07-31 22:34:30 -0400

    added Food class and changed background color

--- snake.cpp
+++ snake.cpp
@@ -1,16 +1,37 @@
 #include <iostream>
 #include <raylib.h>
 
-int main() {
-    const int screenWidth = 800;
-    const int screenHeight = 450;
+Color green = {173,204,96,255};
+Color darkGreen = {43,51,24,255};
 
-    InitWindow(screenWidth, screenHeight, "Snake");
+int cellSize = 30;
+int cellCount = 25;
 
+class Food {
+
+    public:
+        Vector2 position = {5,6};
+
+        void Draw()
+        {
+            DrawRectangle(position.x*cellSize, position.y*cellSize, cellSize, cellSize, darkGreen);
+        }
+};
+
+
+int main() 
+{
+    
+    InitWindow(cellSize*cellCount, cellSize*cellCount, "Classic Snake Game");
     SetTargetFPS(60);
+
+    Food food = Food();
 
     while (!WindowShouldClose()) {
         BeginDrawing();
+
+        ClearBackground(green);
+        food.Draw();
 
         EndDrawing();
     }
